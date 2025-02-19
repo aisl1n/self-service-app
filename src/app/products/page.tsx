08f1b47c41a38ec975ddbe805bb740e0760a303a
aisl1n <aislanarchy@gmail.com>
2025-02-18 21:40:08 -0300

    feat: add initial setup and shadcn components

---
+++ src/app/products/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from '@/components/ui/button';
+import React from 'react';
+
+export default function ProductPage() {
+  return (
+    <div className='p-5 border-red-500 rounded-xl'>
+      <h1 className='text-red-400'>Hello World</h1>
+      <Button>Click me</Button>
+    </div>
+  );
+}
