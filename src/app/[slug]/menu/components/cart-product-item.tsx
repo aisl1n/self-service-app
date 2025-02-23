3ef781147bbc1746ed1aa3b4e2ad25630292e5f3
aisl1n <aislanarchy@gmail.com>
2025-02-22 22:52:38 -0300

    feat: ensure cart product can be removed

--- src/app/[slug]/menu/components/cart-product-item.tsx
+++ src/app/[slug]/menu/components/cart-product-item.tsx
@@ -12,7 +12,7 @@ interface CartProductItemProps {
 }
 
 const CartProductItem = ({ product }: CartProductItemProps) => {
-  const { decreaseProductQuantity, increaseProductQuantity } =
+  const { decreaseProductQuantity, increaseProductQuantity, removeProduct } =
     useContext(CartContext);
   return (
     <div className="flex items-center justify-between">
@@ -45,7 +45,11 @@ const CartProductItem = ({ product }: CartProductItemProps) => {
             </Button>
           </div>
         </div>
-        <Button className="h-7 w-7 rounded-lg" variant="outline">
+        <Button
+          className="h-7 w-7 rounded-lg"
+          variant="outline"
+          onClick={() => removeProduct(product.id)}
+        >
           <TrashIcon />
         </Button>
       </div>
